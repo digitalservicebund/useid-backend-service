e4fb210eee08caa4f35007c6f5a0924fbf8ea607
Klaus Hartl <klaus.hartl@gmail.com>
2022-04-08 11:55:43 +0200

    Start implementing proof of concept
    Spring serves a static html file with a QR code pointing to another
    static file (from there we're going to initiate the "callback" mechanism
    via SSE).

---
+++ src/main/kotlin/de/bund/digitalservice/template/config/SecurityConfig.kt
@@ -0,0 +1,16 @@
+package de.bund.digitalservice.template.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http.authorizeExchange().anyExchange().permitAll().and().build()
+    }
+}
