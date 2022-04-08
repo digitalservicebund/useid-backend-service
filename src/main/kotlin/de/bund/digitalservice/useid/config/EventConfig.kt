78b5ba0da6bcf33208daa5dc39cd5702410da6dc
Klaus Hartl <klaus.hartl@gmail.com>
2022-04-08 16:21:57 +0200

    Reset event sink after a few seconds
    Just for the prototype.. otherwise after the first successful ping we
    will no longer see a QR Code as the sink will replay the success
    notification right away.

--- src/main/kotlin/de/bund/digitalservice/useid/config/EventConfig.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/config/EventConfig.kt
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.core.publisher.Sinks
 import reactor.core.publisher.Sinks.Many
+import java.time.Duration
 
 @Configuration
 class EventConfig {
     @Bean
     fun eventNotifications(): Many<SuccessEvent>? {
-        return Sinks.many().replay().latest()
+        return Sinks.many().replay().limit(Duration.ofSeconds(3))
     }
 }
