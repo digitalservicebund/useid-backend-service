85c580faa89a3e44591f14ce0743c9ec3106934c
Klaus Hartl <klaus.hartl@gmail.com>
2022-04-29 16:07:50 +0200

    Remove obsolete jacoco report configuration
    We do produce a combined report in the end anyway. Removes gradle
    deprecation warning along the way.

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,10 +116,7 @@ tasks {
         val executionDataTree = fileTree(project.buildDir.absolutePath) {
             include("jacoco/*.exec")
         }
-        reports {
-            xml.isEnabled = true
-            html.isEnabled = true
-        }
+
         dependsOn(getByName("integrationTest")) // All tests are required to run before generating a report..
 
         // Avoid config classes skewing coverage..
