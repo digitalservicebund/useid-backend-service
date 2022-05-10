5e014b956da7956eb1f4007b1414e7b2876a41dd
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-05-10 11:42:03 +0200

    Template controller returns a mono

--- src/main/kotlin/de/bund/digitalservice/useid/QRCodeController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/QRCodeController.kt
@@ -3,9 +3,10 @@ package de.bund.digitalservice.useid
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import reactor.core.publisher.Mono
 
 @Controller
 class QRCodeController {
     @GetMapping("/widget")
-    fun widget(model: Model): String = "widget"
+    fun widget(model: Model): Mono<String> = Mono.just("widget")
 }
