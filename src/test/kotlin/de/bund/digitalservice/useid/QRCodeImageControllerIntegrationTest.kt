f0c1d9c66aa81d3c57db5726bda38444b9fe7e87
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-05-10 18:13:49 +0200

    Update the QR-Code fixture

--- src/test/kotlin/de/bund/digitalservice/useid/QRCodeImageControllerIntegrationTest.kt
+++ src/test/kotlin/de/bund/digitalservice/useid/QRCodeImageControllerIntegrationTest.kt
@@ -7,15 +7,16 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Tag("integration")
 class QRCodeImageControllerIntegrationTest(@Autowired val webTestClient: WebTestClient) {
-    private val fixture = ClassPathResource("qr-300-digitalservice-bund-de.png")
-    private val fixtureByteArray = fixture.file.readBytes()
 
     @Test
     fun `should return correct QR Code when the url is encoded`() {
+        val fixture = ClassPathResource("qr-300-digitalservice-bund-de.png")
+        val fixtureByteArray = fixture.file.readBytes()
         webTestClient
             .get()
             .uri("/api/v1/qrcode/300?url=https%3A%2F%2Fdigitalservice.bund.de%2F")
@@ -24,12 +25,14 @@ class QRCodeImageControllerIntegrationTest(@Autowired val webTestClient: WebTest
             .isOk
             .expectHeader()
             .contentType(MediaType.IMAGE_PNG_VALUE)
-//            .expectBody<ByteArray>()
-//            .isEqualTo(fixtureByteArray)
+            .expectBody<ByteArray>()
+            .isEqualTo(fixtureByteArray)
     }
 
     @Test
     fun `should return correct QR Code when the url is not encoded`() {
+        val fixture = ClassPathResource("qr-300-digitalservice-bund-de.png")
+        val fixtureByteArray = fixture.file.readBytes()
         webTestClient
             .get()
             .uri("/api/v1/qrcode/300?url=https://digitalservice.bund.de/")
@@ -38,7 +41,7 @@ class QRCodeImageControllerIntegrationTest(@Autowired val webTestClient: WebTest
             .isOk
             .expectHeader()
             .contentType(MediaType.IMAGE_PNG_VALUE)
-//            .expectBody<ByteArray>()
-//            .isEqualTo(fixtureByteArray)
+            .expectBody<ByteArray>()
+            .isEqualTo(fixtureByteArray)
     }
 }
