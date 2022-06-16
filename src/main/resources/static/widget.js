7f48f818db272b6a2fa178d4747da0146f0ecec0
Simon Kempendorf <simon@code28.de>
2022-06-16 14:41:17 +0200

    Add widget.js to be embedded

---
+++ src/main/resources/static/widget.js
@@ -0,0 +1,29 @@
+function getIframe() {
+  let tcTokenURL = (new URL(document.currentScript.src)).searchParams.get("tcTokenURL");
+  if (tcTokenURL === null) {
+    tcTokenURL = document.getElementById("useid-widget").dataset.tcTokenUrl;
+
+    if (tcTokenURL === undefined) {
+      const error = document.createElement("div");
+      error.innerHTML = "Fehlerhafte Konfiguration: TC Token nicht definiert.";
+      return error;
+    }
+  }
+
+  const iframe = document.createElement("iframe");
+  iframe.setAttribute("src", `https://useid.dev.ds4g.net/widget?tcTokenURL=${tcTokenURL}`);
+  iframe.style.border = "2px solid #B8BDC3";
+  iframe.style.borderRadius = "20px";
+  iframe.style.width = "100%";
+  iframe.style.minHeight = "600px"; // TODO: Set height to fit
+  return iframe;
+}
+
+const container = document.getElementById("useid-widget-container");
+container === null ? document.write(getIframe().outerHTML) : container.appendChild(getIframe());
+
+window.addEventListener("message", (e) => {
+  if (e.origin === 'https://useid.dev.ds4g.net') {
+    location.href = e.data;
+  }
+});
