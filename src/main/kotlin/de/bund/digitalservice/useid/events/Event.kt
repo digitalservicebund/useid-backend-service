26556b61444b6ee201475bf1586d794c4d114557
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-06-24 11:20:20 +0200

    Make all properties of Event required

--- src/main/kotlin/de/bund/digitalservice/useid/events/Event.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/events/Event.kt
@@ -1,7 +1,3 @@
 package de.bund.digitalservice.useid.events
 
-class Event {
-    val consumerId: String? = null
-    val encryptedRefreshAddress: String? = null
-    val widgetSessionId: String? = null
-}
+class Event(val consumerId: String, val encryptedRefreshAddress: String, val widgetSessionId: String)
