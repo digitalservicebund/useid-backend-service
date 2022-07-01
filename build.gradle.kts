0824d86429adcba00d904529f388fbe49c4f8544
Klaus Hartl <klaus.hartl@gmail.com>
2022-07-01 13:59:23 +0200

    Simplify license scan config
    We can rely on provided default transformations and keep only a single
    (normalized) license name.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.jk1.license.filter.LicenseBundleNormalizer
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -209,7 +210,12 @@ spotless {
 }
 
 licenseReport {
-// If there's a new dependency with a yet unknown license causing this task to fail
-// the license(s) will be listed in build/reports/dependency-license/dependencies-without-allowed-license.json
+    // If there's a new dependency with a yet unknown license causing this task to fail
+    // the license(s) will be listed in build/reports/dependency-license/dependencies-without-allowed-license.json
     allowedLicensesFile = File("$projectDir/allowed-licenses.json")
+    filters = arrayOf(
+        // With second arg true we get the default transformations:
+        // https://github.com/jk1/Gradle-License-Report/blob/7cf695c38126b63ef9e907345adab84dfa92ea0e/src/main/resources/default-license-normalizer-bundle.json
+        LicenseBundleNormalizer(null, true),
+    )
 }
