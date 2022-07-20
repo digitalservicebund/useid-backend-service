02aa6162ea7d435d9104df4989d539e63bb22ff1
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-07-20 15:04:07 +0200

    Change data type of sessionId to UUID

--- src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSession.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSession.kt
@@ -1,7 +1,10 @@
 package de.bund.digitalservice.useid.identification
 
+import java.util.UUID
+
 data class IdentificationSession(
     val refreshAddress: String,
     val requestAttributes: List<String>,
+    val sessionId: UUID,
     val tcTokenUrl: String
 )
