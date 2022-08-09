7bc3fc888a80e4acba8788b57699dbbd5c66cc0b
Klaus Hartl <klaus.hartl@gmail.com>
2022-08-09 15:14:34 +0200

    Produce distroless-like container image
    Reduces attack surface as well as size.

--- build.gradle.kts
+++ build.gradle.kts
@@ -153,6 +153,7 @@ tasks {
         val containerImageVersion = System.getenv("CONTAINER_IMAGE_VERSION") ?: "latest"
 
         imageName = "$containerRegistry/$containerImageName:$containerImageVersion"
+        builder = "paketobuildpacks/builder:tiny"
         isPublish = false
 
         docker {
