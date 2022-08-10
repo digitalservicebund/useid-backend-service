f42f6fa440e4525509d6b3852f09d2d8569e6dfd
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-08-10 15:04:26 +0200

    Adapt the data model according the new decision

--- src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSession.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSession.kt
@@ -3,8 +3,8 @@ package de.bund.digitalservice.useid.identification
 import java.util.UUID
 
 data class IdentificationSession(
+    var eIDSessionId: UUID = UUID(0, 0),
     val refreshAddress: String,
     val requestAttributes: List<String>,
-    val sessionId: UUID,
-    val tcTokenUrl: String
+    val useIDSessionId: UUID
 )
