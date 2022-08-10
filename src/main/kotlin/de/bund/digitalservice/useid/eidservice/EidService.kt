55f22ae49ca1925cf54ba940a4ed7486042ade36
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-08-10 15:10:32 +0200

    Add eID package
    - EidService is setting the information needed for an eService to initiate request to eID server
    - EidServiceConfig sets up the necessary information to securely request the request to eID server

---
+++ src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidService.kt
@@ -0,0 +1,41 @@
+package de.bund.digitalservice.useid.eidservice
+
+import de.bund.bsi.eid230.AgeVerificationRequestType
+import de.bund.bsi.eid230.AttributeRequestType
+import de.bund.bsi.eid230.OperationsRequestorType
+import de.bund.bsi.eid230.PlaceVerificationRequestType
+import de.bund.bsi.eid230.UseIDRequestType
+import de.governikus.autent.sdk.eidservice.config.EidServiceConfiguration
+import de.governikus.autent.sdk.eidservice.eidservices.EidService230
+import org.springframework.stereotype.Service
+
+/*
+    The details of data attributes can be found in the TR03130 — BSI eID server technical guideline:
+    https://www.bsi.bund.de/SharedDocs/Downloads/DE/BSI/Publikationen/TechnischeRichtlinien/TR03130/TR-03130_TR-eID-Server_Part1.pdf?__blob=publicationFile&v=3
+    Page: 19 and following
+ */
+@Service
+class EidService private constructor(config: EidServiceConfiguration) : EidService230(config) {
+    // TODO: The data attributes (for example: name, age) request should be configurable for each eService
+    override fun getWebserviceRequest(): UseIDRequestType {
+        val request = UseIDRequestType()
+        val ageVerification = AgeVerificationRequestType()
+        ageVerification.age = 21
+        request.ageVerificationRequest = ageVerification
+
+        val placeVerification = PlaceVerificationRequestType()
+        placeVerification.communityID = "02760503150000"
+        request.placeVerificationRequest = placeVerification
+
+        val selector = OperationsRequestorType()
+        selector.givenNames = AttributeRequestType.ALLOWED
+        selector.ageVerification = AttributeRequestType.ALLOWED
+        selector.placeVerification = AttributeRequestType.REQUIRED
+        selector.placeOfResidence = AttributeRequestType.REQUIRED
+        selector.birthName = AttributeRequestType.ALLOWED
+        selector.residencePermitI = AttributeRequestType.ALLOWED
+        request.useOperations = selector
+
+        return request
+    }
+}
---
+++ src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidServiceConfig.kt
@@ -0,0 +1,202 @@
+package de.bund.digitalservice.useid.eidservice
+
+import de.governikus.autent.key.utils.KeyStoreSupporter
+import de.governikus.autent.sdk.eidservice.config.EidServiceConfiguration
+import de.governikus.autent.sdk.eidservice.exceptions.SslConfigException
+import de.governikus.autent.sdk.eidservice.wrapper.KeyStoreAccessor
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import java.io.IOException
+import java.security.KeyStore
+import java.security.cert.CertificateException
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.xml.ws.BindingProvider
+
+@Configuration
+class EidServiceConfig : EidServiceConfiguration {
+    /**
+     * This value holds the URL where to download the WSDL.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${autent.eidservice.wsdl.url}")
+    private lateinit var wsdlUrl: String
+
+    /**
+     * This value holds the URL of the eID service.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${autent.eidservice.url}")
+    private lateinit var serviceUrl: String
+
+    /**
+     * This value holds the path to the TLS certificate of the Autent server.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${autent.eidservice.tls.path}")
+    private lateinit var autentTlsCertPath: String
+
+    /**
+     * This value holds the password to the TLS certificate keystore of the Autent server.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${autent.keystore.password}")
+    private lateinit var autentTlsCertPassword: String
+
+    /**
+     * This value holds the path to the keystore that should be used for the TLS client authentication.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${tls.keystore.path}")
+    private lateinit var tlsKeystorePath: String
+
+    /**
+     * This value holds the type of the keystore that should be used for the TLS client authentication. Possible
+     * values: `JKS` and `PKCS12`.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${tls.keystore.type}")
+    private lateinit var tlsKeystoreType: String
+
+    /**
+     * This value holds the alias of the key that should be used for the TLS client authentication.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${tls.keystore.alias}")
+    private lateinit var tlsKeystoreAlias: String
+
+    /**
+     * This value holds the password to access the keystore that should be used for the TLS client
+     * authentication.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${tls.keystore.password}")
+    private lateinit var tlsKeystorePassword: String
+
+    /**
+     * This value holds the password to access the key for alias [.sigKeystoreAlias] that should be used
+     * for the TLS client authentication.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${tls.keystore.key.password}")
+    private lateinit var tlsKeystoreKeyPassword: String
+
+    /**
+     * This value holds the path to the signature certificate of the Autent server.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${autent.eidservice.sig.path}")
+    private lateinit var autentSigCertPath: String
+
+    /**
+     * This value holds the path to the keystore that should be used for the XML signature.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${xmlsig.keystore.path}")
+    private lateinit var xmlSignatureKeystorePath: String
+
+    /**
+     * This value holds the type of the keystore that should be used for the XML signature. Possible values:
+     * `JKS` and `PKCS12`.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${xmlsig.keystore.type}")
+    private lateinit var sigKeystoreType: String
+
+    /**
+     * This value holds the alias of the key that should be used for the XML signature.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${xmlsig.keystore.alias}")
+    private lateinit var sigKeystoreAlias: String
+
+    /**
+     * This value holds the password to access the keystore that should be used for the XML signature.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${xmlsig.keystore.password}")
+    private lateinit var sigKeystorePassword: String
+
+    /**
+     * This value holds the password to access the key for alias [.sigKeystoreAlias] that should be used
+     * for the XML signature.
+     *
+     * @see "classpath:application.yaml"
+     */
+    @Value("\${xmlsig.keystore.key.password}")
+    private lateinit var sigKeystoreKeyPassword: String
+
+    override fun getEidServiceWsdlUrl(): String = wsdlUrl
+
+    override fun getEidServiceUrl(): String = serviceUrl
+
+    override fun getTruststore(): KeyStore {
+        // TODO: Do we need to securely store the keystorePassword?
+        return KeyStoreSupporter.toKeyStore(
+            readCertificate(autentTlsCertPath),
+            "eid",
+            autentTlsCertPassword,
+            KeyStoreSupporter.KeyStoreType.JKS
+        )
+    }
+
+    override fun getXmlSignatureVerificationCertificate(): X509Certificate = readCertificate(autentSigCertPath)
+
+    override fun getXmlSignatureCreationKeystore(): KeyStoreAccessor {
+        val sigKeyStore = KeyStoreSupporter.readKeyStore(
+            ClassPathResource(xmlSignatureKeystorePath).inputStream,
+            KeyStoreSupporter.KeyStoreType.valueOf(sigKeystoreType),
+            sigKeystorePassword
+        )
+        return KeyStoreAccessor(sigKeyStore, sigKeystorePassword, sigKeystoreAlias, sigKeystoreKeyPassword)
+    }
+
+    override fun getSslKeystoreForMutualTlsAuthentication(): KeyStoreAccessor {
+        val tlsKeyStore = KeyStoreSupporter.readKeyStore(
+            ClassPathResource(tlsKeystorePath).inputStream,
+            KeyStoreSupporter.KeyStoreType.valueOf(tlsKeystoreType),
+            tlsKeystorePassword
+        )
+        return KeyStoreAccessor(tlsKeyStore, tlsKeystorePassword, tlsKeystoreAlias, tlsKeystoreKeyPassword)
+    }
+
+    override fun configureEidPort(eidPort: BindingProvider) {
+        // No implementation
+    }
+
+    /**
+     * Read a certificate from the classpath.
+     *
+     * @param path classpath to the certificate
+     * @return the certificate
+     */
+    private fun readCertificate(path: String): X509Certificate {
+        try {
+            val certificateResource = ClassPathResource(path)
+            val certFactory = CertificateFactory.getInstance("X.509")
+
+            return certificateResource.inputStream.use {
+                certFactory.generateCertificate(it) as X509Certificate
+            }
+        } catch (e: CertificateException) {
+            throw SslConfigException(e)
+        } catch (e: IOException) {
+            throw SslConfigException(e)
+        }
+    }
+}
