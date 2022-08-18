4ae84fc78bc8d7858215483e6ac9eeef33469dbe
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-08-18 09:02:49 +0200

    Revert encoding tcTokenUrl

--- src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSessionsController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSessionsController.kt
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Mono
-import reactor.core.scheduler.Schedulers
 import java.util.UUID
 
 internal const val IDENTIFICATION_SESSIONS_BASE_PATH = "/api/v1/identification/sessions"
@@ -52,16 +51,15 @@ class IdentificationSessionsController(
                 refreshAddress = (authentication as ApiKeyAuthenticationToken).details.refreshAddress!!,
                 requestAttributes = createIdentitySessionRequest.requestAttributes
             )
-            .publishOn(Schedulers.boundedElastic())
             .map {
                 /* TODO: Find a way to build the url, by using serverHttpRequest.uri is not preferable
                     since the path parameter can be different (in case of in proxy server)
                  */
-                val encodedTcTokenUrl = "${serverHttpRequest.uri}/${it.useIDSessionId}/$TCTOKEN_PATH_SUFFIX"
+                val tcTokenUrl = "${serverHttpRequest.uri}/${it.useIDSessionId}/$TCTOKEN_PATH_SUFFIX"
                 ResponseEntity
                     .status(HttpStatus.OK)
                     .contentType(MediaType.APPLICATION_JSON)
-                    .body(CreateIdentitySessionResponse(tcTokenUrl = encodedTcTokenUrl))
+                    .body(CreateIdentitySessionResponse(tcTokenUrl = tcTokenUrl))
             }
             .doOnError { exception ->
                 log.error {
