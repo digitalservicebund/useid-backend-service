393b1758227252f3997d7f3a4a7c8678f50b0e71
Simon Kempendorf <s.kempendorf@tice-software.com>
2022-08-30 09:06:53 +0200

    Fix tc token endpoint for now

--- src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidService.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidService.kt
@@ -1,13 +1,10 @@
 package de.bund.digitalservice.useid.eidservice
 
-import de.bund.bsi.eid230.AttributeRequestType
 import de.bund.bsi.eid230.OperationsRequestorType
 import de.bund.bsi.eid230.UseIDRequestType
-import de.bund.digitalservice.useid.apikeys.ApiKeyDetails
 import de.governikus.autent.sdk.eidservice.config.EidServiceConfiguration
 import de.governikus.autent.sdk.eidservice.eidservices.EidService230
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 /*
@@ -19,40 +16,39 @@ import org.springframework.stereotype.Service
 @ConditionalOnProperty("feature.eid-service-integration.enabled", havingValue = "true")
 class EidService private constructor(config: EidServiceConfiguration) : EidService230(config) {
     public override fun getWebserviceRequest(): UseIDRequestType {
-        val requestDataGroups = (SecurityContextHolder.getContext().authentication?.details as ApiKeyDetails?)?.requestDataGroups
+        // val requestDataGroups = emptyList<String>() // TODO: Get dataGroups for this eService
         val request = UseIDRequestType()
+        val selector = OperationsRequestorType()
 
         // Data groups are only one function of the eID.
         // Further operators like age verification etc. can be added for further use cases.
-        if (requestDataGroups !== null && requestDataGroups.isNotEmpty()) {
-            val selector = OperationsRequestorType()
-
-            for (dataGroup in requestDataGroups) {
-                when (dataGroup) {
-                    "DG1" -> selector.documentType = AttributeRequestType.REQUIRED
-                    "DG2" -> selector.issuingState = AttributeRequestType.REQUIRED
-                    "DG3" -> selector.dateOfExpiry = AttributeRequestType.REQUIRED
-                    "DG4" -> selector.givenNames = AttributeRequestType.REQUIRED
-                    "DG5" -> selector.familyNames = AttributeRequestType.REQUIRED
-
-                    "DG7" -> selector.academicTitle = AttributeRequestType.REQUIRED
-                    "DG8" -> selector.dateOfBirth = AttributeRequestType.REQUIRED
-                    "DG9" -> selector.placeOfBirth = AttributeRequestType.REQUIRED
-                    "DG10" -> selector.nationality = AttributeRequestType.REQUIRED
-
-                    "DG13" -> selector.birthName = AttributeRequestType.REQUIRED
-
-                    "DG17" -> selector.placeOfResidence = AttributeRequestType.REQUIRED
-
-                    "DG19" -> selector.residencePermitI = AttributeRequestType.REQUIRED
-
-                    else -> {
-                        throw IllegalStateException("Invalid data group for this eService")
-                    }
-                }
-            }
-            request.useOperations = selector
-        }
+        // if (requestDataGroups !== null && requestDataGroups.isNotEmpty()) {
+        //     for (dataGroup in requestDataGroups) {
+        //         when (dataGroup) {
+        //             "DG1" -> selector.documentType = AttributeRequestType.REQUIRED
+        //             "DG2" -> selector.issuingState = AttributeRequestType.REQUIRED
+        //             "DG3" -> selector.dateOfExpiry = AttributeRequestType.REQUIRED
+        //             "DG4" -> selector.givenNames = AttributeRequestType.REQUIRED
+        //             "DG5" -> selector.familyNames = AttributeRequestType.REQUIRED
+        //
+        //             "DG7" -> selector.academicTitle = AttributeRequestType.REQUIRED
+        //             "DG8" -> selector.dateOfBirth = AttributeRequestType.REQUIRED
+        //             "DG9" -> selector.placeOfBirth = AttributeRequestType.REQUIRED
+        //             "DG10" -> selector.nationality = AttributeRequestType.REQUIRED
+        //
+        //             "DG13" -> selector.birthName = AttributeRequestType.REQUIRED
+        //
+        //             "DG17" -> selector.placeOfResidence = AttributeRequestType.REQUIRED
+        //
+        //             "DG19" -> selector.residencePermitI = AttributeRequestType.REQUIRED
+        //
+        //             else -> {
+        //                 throw IllegalStateException("Invalid data group for this eService")
+        //             }
+        //         }
+        //     }
+        // }
+        request.useOperations = selector
         return request
     }
 }
