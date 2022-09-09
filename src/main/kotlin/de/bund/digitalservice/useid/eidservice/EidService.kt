95c8ba89e605109f909072ff6aaaa39a7a707d51
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2022-09-09 16:14:47 +0200

    Added mapping logic and tests

--- src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidService.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidService.kt
@@ -1,5 +1,6 @@
 package de.bund.digitalservice.useid.eidservice
 
+import de.bund.bsi.eid230.AttributeRequestType
 import de.bund.bsi.eid230.OperationsRequestorType
 import de.bund.bsi.eid230.UseIDRequestType
 import de.governikus.autent.sdk.eidservice.config.EidServiceConfiguration
@@ -28,39 +29,35 @@ class EidService constructor(config: EidServiceConfiguration) : EidService230(co
 
     var dataGroups: List<String> = emptyList<String>()
     public override fun getWebserviceRequest(): UseIDRequestType {
-        // TODO: Get dataGroups for this eService
-
         val request = UseIDRequestType()
         val selector = OperationsRequestorType()
 
-        // Data groups are only one function of the eID.
-        // Further operators like age verification etc. can be added for further use cases.
-        // if (requestDataGroups !== null && requestDataGroups.isNotEmpty()) {
-        //     for (dataGroup in requestDataGroups) {
-        //         when (dataGroup) {
-        //             "DG1" -> selector.documentType = AttributeRequestType.REQUIRED
-        //             "DG2" -> selector.issuingState = AttributeRequestType.REQUIRED
-        //             "DG3" -> selector.dateOfExpiry = AttributeRequestType.REQUIRED
-        //             "DG4" -> selector.givenNames = AttributeRequestType.REQUIRED
-        //             "DG5" -> selector.familyNames = AttributeRequestType.REQUIRED
-        //
-        //             "DG7" -> selector.academicTitle = AttributeRequestType.REQUIRED
-        //             "DG8" -> selector.dateOfBirth = AttributeRequestType.REQUIRED
-        //             "DG9" -> selector.placeOfBirth = AttributeRequestType.REQUIRED
-        //             "DG10" -> selector.nationality = AttributeRequestType.REQUIRED
-        //
-        //             "DG13" -> selector.birthName = AttributeRequestType.REQUIRED
-        //
-        //             "DG17" -> selector.placeOfResidence = AttributeRequestType.REQUIRED
-        //
-        //             "DG19" -> selector.residencePermitI = AttributeRequestType.REQUIRED
-        //
-        //             else -> {
-        //                 throw IllegalStateException("Invalid data group for this eService")
-        //             }
-        //         }
-        //     }
-        // }
+        // Data groups are only one function of the eID
+        // Further operators like age verification etc. can be added for further use cases
+        dataGroups.forEach {
+            when (it) {
+                "DG1" -> selector.documentType = AttributeRequestType.REQUIRED
+                "DG2" -> selector.issuingState = AttributeRequestType.REQUIRED
+                "DG3" -> selector.dateOfExpiry = AttributeRequestType.REQUIRED
+                "DG4" -> selector.givenNames = AttributeRequestType.REQUIRED
+                "DG5" -> selector.familyNames = AttributeRequestType.REQUIRED
+
+                "DG7" -> selector.academicTitle = AttributeRequestType.REQUIRED
+                "DG8" -> selector.dateOfBirth = AttributeRequestType.REQUIRED
+                "DG9" -> selector.placeOfBirth = AttributeRequestType.REQUIRED
+                "DG10" -> selector.nationality = AttributeRequestType.REQUIRED
+
+                "DG13" -> selector.birthName = AttributeRequestType.REQUIRED
+
+                "DG17" -> selector.placeOfResidence = AttributeRequestType.REQUIRED
+
+                "DG19" -> selector.residencePermitI = AttributeRequestType.REQUIRED
+
+                else -> {
+                    throw IllegalStateException("Invalid data group for this eService")
+                }
+            }
+        }
 
         request.useOperations = selector
         return request
--- src/test/kotlin/de/bund/digitalservice/useid/eidservice/EidServiceTest.kt
+++ src/test/kotlin/de/bund/digitalservice/useid/eidservice/EidServiceTest.kt
@@ -1,9 +1,12 @@
 package de.bund.digitalservice.useid.eidservice
 
+import de.bund.bsi.eid230.AttributeRequestType
 import de.bund.bsi.eid230.UseIDRequestType
 import de.governikus.autent.sdk.eidservice.config.EidServiceConfiguration
 import org.hamcrest.CoreMatchers.isA
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,5 +26,25 @@ class EidServiceTest {
         val eidService = EidService(config)
         val webserviceRequest = eidService.webserviceRequest
         assertThat(webserviceRequest, isA(UseIDRequestType::class.java))
+    }
+
+    @Test
+    fun `getWebserviceRequest includes specified data groups as part of operations in request`() {
+        val eidService = EidService(config)
+        eidService.dataGroups = listOf("DG1", "DG4", "DG10")
+        val operations = eidService.webserviceRequest.useOperations
+        val required = AttributeRequestType.REQUIRED
+        assertEquals(operations.documentType, required)
+        assertEquals(operations.givenNames, required)
+        assertEquals(operations.nationality, required)
+    }
+
+    @Test
+    fun `getWebserviceRequest throws IllegalStateException when passed invalid list items as data groups`() {
+        val eidService = EidService(config)
+        eidService.dataGroups = listOf("iAmInvalid", "meToo")
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            eidService.webserviceRequest.useOperations
+        }
     }
 }
