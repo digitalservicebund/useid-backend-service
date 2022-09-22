de2f38ab660fefb699e6de7d440672138b361071
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-09-22 15:15:04 +0200

    Add endpoint for ios universal link config file

---
+++ src/main/kotlin/de/bund/digitalservice/useid/statics/DeeplinkController.kt
@@ -0,0 +1,38 @@
+package de.bund.digitalservice.useid.statics
+
+import mu.KotlinLogging
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import reactor.core.publisher.Mono
+
+@Controller
+class DeeplinkController {
+
+    private val log = KotlinLogging.logger {}
+
+    @GetMapping(
+        path = [".well-known/apple-app-site-association"],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun getIOSUniversalLink(): Mono<ResponseEntity<IOSUniversalLink>> {
+        val appIds = listOf(
+            "VDTVKQ35RL.de.bund.digitalservice.UseID",
+            "VDTVKQ35RL.de.bund.digitalservice.UseID-Preview"
+        )
+        val components = listOf(Component(pathUrlName = "/eID-Client"))
+        val details = listOf(Details(appIds, components))
+        val universalLink = UniversalLink(details)
+
+        return Mono.just(
+            ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(IOSUniversalLink(universalLink))
+        )
+            .doOnError {
+                log.error("Failed to return iOS Universal Link config", it)
+            }
+            .onErrorReturn(
+                ResponseEntity.internalServerError().build()
+            )
+    }
+}
---
+++ src/test/kotlin/de/bund/digitalservice/useid/statics/DeeplinkControllerIntegrationTest.kt
@@ -0,0 +1,25 @@
+package de.bund.digitalservice.useid.statics
+
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Tag("integration")
+class DeeplinkControllerIntegrationTest(@Autowired val webTestClient: WebTestClient) {
+
+    @Test
+    fun `iOS deeplink endpoint returns JSON file`() {
+        webTestClient
+            .get()
+            .uri("/.well-known/apple-app-site-association")
+            .exchange()
+            .expectStatus()
+            .isOk
+            .expectBody()
+            .jsonPath("$.applinks.details[0].appIDs").exists()
+            .jsonPath("$.applinks.details[0].components").exists()
+    }
+}
