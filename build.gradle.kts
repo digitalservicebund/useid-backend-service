88b11bf077068da43bce09dc2c3dd0ff25324abd
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-09-28 08:52:39 +0200

    Pin version of postgresql due to CVE-2022-31197

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
 
     /** Persistence **/
     implementation("org.springframework.data:spring-data-r2dbc")
-    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.postgresql:postgresql:42.4.1") // Pin version due to CVE-2022-31197
     runtimeOnly("org.postgresql:r2dbc-postgresql")
     implementation("org.flywaydb:flyway-core:9.3.0")
 
