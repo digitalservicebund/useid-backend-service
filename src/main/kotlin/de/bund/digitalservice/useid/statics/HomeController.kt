fe2925bf32ea9f53e4b1fee7df83a48e89ed0919
Patrick Huesler <patrick.huesler@gmail.com>
2022-09-29 10:56:22 +0200

    Add prometheus

--- src/main/kotlin/de/bund/digitalservice/useid/statics/HomeController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/statics/HomeController.kt
@@ -1,10 +1,12 @@
 package de.bund.digitalservice.useid.statics
 
+import io.micrometer.core.annotation.Timed
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import reactor.core.publisher.Mono
 
 @Controller
+@Timed
 class HomeController {
     @GetMapping("/")
     fun home(): Mono<String> = Mono.just("redirect:https://digitalservice.bund.de")
