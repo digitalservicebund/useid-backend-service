d9890ff5fca0a2e52d6fe578e69e7f71880dc5ef
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-09-30 16:09:55 +0200

    Add auditing for identification session entity
    Add created_at and updated_at timestamp.

--- src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSession.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSession.kt
@@ -1,8 +1,11 @@
 package de.bund.digitalservice.useid.identification
 
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Table("identification_session")
@@ -21,4 +24,12 @@ data class IdentificationSession(
 
     @Column("eid_session_id")
     var eidSessionId: UUID? = null
+
+    @Column("created_at")
+    @CreatedDate
+    var createdAt: LocalDateTime? = null
+
+    @Column("updated_at")
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null
 }
--- src/main/resources/db/migration/V1__Create_Identification_Session_Table.sql
+++ src/main/resources/db/migration/V1__Create_Identification_Session_Table.sql
@@ -4,5 +4,7 @@ CREATE TABLE identification_session
     useid_session_id UUID NOT NULL UNIQUE, -- PostgreSQL automatically creates an index on columns with unique constraint
     refresh_address VARCHAR NOT NULL,
     request_data_groups VARCHAR(5)[] NOT NULL,
-    eid_session_id  UUID UNIQUE
+    eid_session_id  UUID UNIQUE,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
