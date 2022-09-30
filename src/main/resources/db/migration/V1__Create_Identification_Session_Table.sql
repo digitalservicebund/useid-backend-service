ecf9608f387bbc97b2af57b4261662224e7c6e51
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-09-30 15:55:45 +0200

    Add comment regarding indices to migration

--- src/main/resources/db/migration/V1__Create_Identification_Session_Table.sql
+++ src/main/resources/db/migration/V1__Create_Identification_Session_Table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE identification_session
 (
     id SERIAL PRIMARY KEY,
-    useid_session_id UUID NOT NULL UNIQUE,
+    useid_session_id UUID NOT NULL UNIQUE, -- PostgreSQL automatically creates an index on columns with unique constraint
     refresh_address VARCHAR NOT NULL,
     request_data_groups VARCHAR(5)[] NOT NULL,
     eid_session_id  UUID UNIQUE
