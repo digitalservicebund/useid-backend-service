af2af575505cd1a2f5daf2b9253aa4eb3e0a866c
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2022-10-24 16:43:04 +0200

    Added attachButtonTracking.js

--- src/main/resources/static/js/attachButtonTracking.js
+++ src/main/resources/static/js/attachButtonTracking.js
@@ -1,7 +1,9 @@
-document.getElementById("eid-client-button").addEventListener("click", async function () {
-    await fetch('/app-opened', {
-        cache: "no-store",
-        method: "POST",
-        keepalive: true
+document
+  .getElementById("eid-client-button")
+  .addEventListener("click", async function () {
+    await fetch("/app-opened", {
+      cache: "no-store",
+      method: "POST",
+      keepalive: true,
     });
-});
+  });
