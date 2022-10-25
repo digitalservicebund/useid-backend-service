197663a9b081bb0622807d1cd3ad195c952997ba
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-10-25 11:40:37 +0200

    Add endpoint to trigger test exception for sentry

--- src/main/kotlin/de/bund/digitalservice/useid/statics/HomeController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/statics/HomeController.kt
@@ -4,10 +4,16 @@ import io.micrometer.core.annotation.Timed
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import reactor.core.publisher.Mono
+import java.util.Date
 
 @Controller
 @Timed
 class HomeController {
     @GetMapping("/")
     fun home(): Mono<String> = Mono.just("redirect:https://digitalservice.bund.de")
+
+    @GetMapping("/boom")
+    fun testSentryIntegration(): Mono<Void> {
+        throw Exception("This is a test exception for Sentry thrown at ${Date()}")
+    }
 }
