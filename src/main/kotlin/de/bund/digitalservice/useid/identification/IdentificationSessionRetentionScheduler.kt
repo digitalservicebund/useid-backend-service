e2c3c2ad86d04fba2aef7ee65207b8feb92ad845
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-10-26 18:24:03 +0200

    Add test for scheduler

--- src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSessionRetentionScheduler.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSessionRetentionScheduler.kt
@@ -6,13 +6,14 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime.now
 
-private const val RETENTION_IN_DAYS: Long = 7
+internal const val RETENTION_IN_DAYS: Long = 7
+private const val EVERY_DAY_AT_MIDNIGHT: String = "0 0 0 * * *"
 
 @Component
 class IdentificationSessionRetentionScheduler(val identificationSessionRepository: IdentificationSessionRepository) {
     private val log = KotlinLogging.logger {}
 
-    @Scheduled(cron = "0 0 0 * * *") // every day at midnight
+    @Scheduled(cron = EVERY_DAY_AT_MIDNIGHT)
     @SchedulerLock(name = "identificationSessionRetentionCleanup")
     fun cleanupExpiredIdentificationSessionFromDatabase() {
         log.info("Cleanup identification sessions older than $RETENTION_IN_DAYS days.")
