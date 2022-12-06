c97faab75c9f6406201df35c8ff362433408c76f
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2022-12-06 16:30:54 +0100

    USEID-583: Custom 4xx and 5xx pages (#145)
    - Custom 4xx and 5xx by overriding the attributes error with default value and custom value
    - Use UrlUtils.encode() that encodes whitespace to %20, Java URLEncoder.encode() encodes whitespace to + sign, both are valid, but for our case, we need to use %20

---
+++ src/main/kotlin/de/bund/digitalservice/useid/statics/GlobalStaticError.kt
@@ -0,0 +1,49 @@
+package de.bund.digitalservice.useid.statics
+
+import de.bund.digitalservice.useid.config.ApplicationProperties
+import de.bund.digitalservice.useid.widget.WidgetProperties
+import org.springframework.boot.web.error.ErrorAttributeOptions
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.util.UriUtils
+import kotlin.text.Charsets.UTF_8
+
+@Component
+class GlobalStaticError(
+    private val applicationProperties: ApplicationProperties,
+    private val globalStaticProperties: GlobalStaticProperties,
+    private val widgetProperties: WidgetProperties
+) : DefaultErrorAttributes() {
+
+    override fun getErrorAttributes(request: ServerRequest, options: ErrorAttributeOptions): Map<String, Any> {
+        val errorAttributes = super.getErrorAttributes(request, options)
+        val statusCode = errorAttributes["status"] as Int
+
+        val customGlobalErrorAttributes = mapOf(
+            "localizationError" to globalStaticProperties.errorView.localization,
+            "errorReportEmailLink" to createEmailReportLink(statusCode),
+            "baseUrl" to applicationProperties.baseUrl,
+            "metaTag" to widgetProperties.metaTag
+        )
+
+        return errorAttributes + customGlobalErrorAttributes
+    }
+
+    private fun createEmailReportLink(statusCode: Int): String {
+        val emailAddress = globalStaticProperties.errorView.localization.errorReportEmail
+        val errorReportSubject = globalStaticProperties.errorView.localization.errorReportSubject
+        val errorReportBody = globalStaticProperties.errorView.localization.errorReportBody
+
+        /*
+            URLEncoder.encode() will encode whitespace to "+" instead of %20 which will not work for email link,
+            it would replace whitespace with "+" sign in the e-mail body. UriUtils.encode() from Spring encodes
+            whitespace to "%20"
+            RFC Document: https://www.rfc-editor.org/rfc/rfc6068#section-5
+         */
+        val encodedEmailSubject = UriUtils.encode("$errorReportSubject $statusCode", UTF_8)
+        val encodedBody = UriUtils.encode(errorReportBody, UTF_8)
+
+        return "mailto:$emailAddress?subject=$encodedEmailSubject&body=$encodedBody"
+    }
+}
---
+++ src/main/kotlin/de/bund/digitalservice/useid/statics/GlobalStaticProperties.kt
@@ -0,0 +1,41 @@
+package de.bund.digitalservice.useid.statics
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+import org.springframework.validation.annotation.Validated
+import javax.validation.Valid
+import javax.validation.constraints.NotBlank
+
+@Component
+@ConfigurationProperties(prefix = "global-static")
+@Validated
+class GlobalStaticProperties {
+
+    @Valid
+    var errorView: ErrorView = ErrorView()
+
+    class ErrorView {
+        @Valid
+        var localization: Localization = Localization()
+
+        class Localization {
+            @NotBlank
+            lateinit var iconAlt: String
+
+            @NotBlank
+            lateinit var errorTitle: String
+
+            @NotBlank
+            lateinit var errorText: String
+
+            @NotBlank
+            lateinit var errorReportEmail: String
+
+            @NotBlank
+            lateinit var errorReportSubject: String
+
+            @NotBlank
+            lateinit var errorReportBody: String
+        }
+    }
+}
