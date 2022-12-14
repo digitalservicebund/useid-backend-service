f82aaa1d1f16d6c1aea6bbfebfb39340df20cd1b
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-12-14 15:27:58 +0100

    Add working example of SSE to eventSource.js

--- src/main/resources/static/js/eventSource.js
+++ src/main/resources/static/js/eventSource.js
@@ -1,6 +1,26 @@
-const eventSource = new EventSource("/v1/sse");
+function subscribe(consumer) {
+  const eventSource = new EventSource("/api/v1/events/" + consumer);
+  eventSource.addEventListener("success", function (event) {
+    console.log("Received success event for " + consumer + ": " + event.data);
 
-eventSource.addEventListener("close", (event) => {
-  console.log(event.data);
-  eventSource.close();
-});
+    if (event.origin !== "http://localhost:8080") {
+      return;
+    }
+
+    console.log(event);
+    // TODO do stuff
+  });
+
+  eventSource.addEventListener("close", (event) => {
+    console.log("Received close event for " + consumer + ": " + event.data);
+
+    if (event.origin !== "http://localhost:8080") {
+      return;
+    }
+
+    eventSource.close();
+  });
+}
+
+subscribe("00000000-0000-0000-0000-000000000001");
+subscribe("00000000-0000-0000-0000-000000000002");
