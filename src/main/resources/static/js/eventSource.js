fa3265b306cf0feb139567ff7067360b0bbfa860
Simon Kempendorf <s.kempendorf@tice-software.com>
2022-12-15 13:24:18 +0100

    Remove origin check on event source
    It's only needed for messages sent via postMessage and not for messages sent from an EventSource, since we're only subscribing to a specific domain

--- src/main/resources/static/js/eventSource.js
+++ src/main/resources/static/js/eventSource.js
@@ -2,22 +2,12 @@ function subscribe(consumer) {
   const eventSource = new EventSource("/api/v1/events/" + consumer);
   eventSource.addEventListener("success", function (event) {
     console.log("Received success event for " + consumer + ": " + event.data);
-
-    if (event.origin !== "http://localhost:8080") {
-      return;
-    }
-
     console.log(event);
     // TODO do stuff
   });
 
   eventSource.addEventListener("close", (event) => {
     console.log("Received close event for " + consumer + ": " + event.data);
-
-    if (event.origin !== "http://localhost:8080") {
-      return;
-    }
-
     eventSource.close();
   });
 }
