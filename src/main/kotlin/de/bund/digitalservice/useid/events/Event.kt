bf2ddf58ad3b2f4d3bfcfa9f97697b53f990c487
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-12-16 15:29:02 +0100

    Rename event parameter

--- src/main/kotlin/de/bund/digitalservice/useid/events/Event.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/events/Event.kt
@@ -2,4 +2,4 @@ package de.bund.digitalservice.useid.events
 
 import java.util.UUID
 
-data class Event(val widgetSessionId: UUID, val encryptedRefreshAddress: String)
+data class Event(val widgetSessionId: UUID, val refreshAddress: String)
