7677ee1acf30fdb7ee1c490982e2ba53227650cd
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-01-12 12:27:10 +0100

    Remove unneeded parameter

--- src/main/kotlin/de/bund/digitalservice/useid/transactioninfo/TransactionInfoController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/transactioninfo/TransactionInfoController.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -31,8 +30,7 @@ class TransactionInfoController(
     )
     fun createTransactionInfo(
         @PathVariable useIDSessionId: UUID,
-        @RequestBody transactionInfo: TransactionInfo,
-        authentication: Authentication
+        @RequestBody transactionInfo: TransactionInfo
     ): Mono<ResponseEntity<TransactionInfo>> {
         return transactionInfoService.create(useIDSessionId, transactionInfo)
             .map { TransactionInfo.fromDto(it) }
