bd7852f5ff80d09a7c83c8c4bd75dd55f820cdfb
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-01-13 10:32:08 +0100

    NO-TICKET: Bump version of netty-all
    Due to detected vulnerability CVE-2022-41881.

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
         because("CVE-2021-43797, not using Tomcat")
     }
     // => CVE-2021-37136, CVE-2021-37137, CVE-2021-43797
-    implementation("io.netty:netty-all:4.1.77.Final") {
+    implementation("io.netty:netty-all:4.1.87.Final") {
         exclude(group = "io.netty", module = "netty-tcnative-classes")
         because("CVE-2021-43797, not using Tomcat")
     }
