7d5257ae6df3c009372318726c33a8ed8409dcbb
Simon Kempendorf <s.kempendorf@tice-software.com>
2023-01-18 10:21:18 +0100

    NO-TICKET: Fix and complete API docs (#183)

--- src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSessionController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/identification/IdentificationSessionController.kt
@@ -10,6 +10,13 @@ import de.governikus.autent.sdk.eidservice.config.EidServiceConfiguration
 import de.governikus.autent.sdk.eidservice.tctoken.TCTokenType
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Metrics
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.tags.Tag
 import mu.KotlinLogging
 import org.springframework.http.HttpStatus
@@ -33,6 +40,7 @@ internal const val TCTOKEN_PATH_SUFFIX = "tc-token"
 @RestController
 @RequestMapping(IDENTIFICATION_SESSIONS_BASE_PATH)
 @Tag(name = "Identification Sessions", description = "An identification session represent an ongoing identification flow of a user and stores the required information.")
+@SecurityScheme(type = SecuritySchemeType.HTTP, name = "apiKey", `in` = SecuritySchemeIn.HEADER, paramName = "Authorization", scheme = "Bearer", description = "API key as bearer token in `Authorization` header")
 class IdentificationSessionsController(
     private val identificationSessionService: IdentificationSessionService,
     private val applicationProperties: ApplicationProperties,
@@ -45,6 +53,10 @@ class IdentificationSessionsController(
     private val getEidInformationCallsWithErrorsCounter: Counter = Metrics.counter(METRIC_NAME_EID_SERVICE_REQUESTS, "method", "get_eid_information", "status", "500")
 
     @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Operation(summary = "Create session as eService")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "401", description = "Authentication failed (missing or wrong api key)", content = [Content()])
+    @SecurityRequirement(name = "apiKey")
     fun createSession(
         authentication: Authentication
     ): Mono<ResponseEntity<CreateIdentificationSessionResponse>> {
@@ -67,6 +79,9 @@ class IdentificationSessionsController(
         path = ["/{useIdSessionId}/$TCTOKEN_PATH_SUFFIX"],
         produces = [MediaType.APPLICATION_XML_VALUE]
     )
+    @Operation(summary = "Get TC token for this session")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "404", description = "No corresponding session found for that useIdSessionId", content = [Content()])
     fun getTCToken(@PathVariable useIdSessionId: UUID): Mono<ResponseEntity<TCTokenType>> {
         return identificationSessionService.findByUseIdSessionId(useIdSessionId)
             .flatMap {
@@ -103,7 +118,12 @@ class IdentificationSessionsController(
             )
     }
 
-    @GetMapping("/{eIdSessionId}")
+    @GetMapping("/{eIdSessionId}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Operation(summary = "Fetch data as eService after identification was successful")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "404", description = "No corresponding session found for that eIdSessionId", content = [Content()])
+    @ApiResponse(responseCode = "401", description = "Authentication failed (missing or wrong api key)", content = [Content()])
+    @SecurityRequirement(name = "apiKey")
     fun getIdentity(@PathVariable eIdSessionId: UUID, authentication: Authentication): Mono<ResponseEntity<GetResultResponseType>> {
         /*
             Wrapping blocking call to the SDK into Mono.fromCallable
--- src/main/kotlin/de/bund/digitalservice/useid/timebasedtokens/TimeBasedTokenController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/timebasedtokens/TimeBasedTokenController.kt
@@ -2,6 +2,7 @@ package de.bund.digitalservice.useid.timebasedtokens
 
 import de.bund.digitalservice.useid.identification.IDENTIFICATION_SESSIONS_BASE_PATH
 import io.micrometer.core.annotation.Timed
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -24,6 +25,7 @@ import java.util.UUID
 @ConditionalOnProperty(name = ["features.desktop-solution-enabled"], havingValue = "true")
 class TimeBasedTokenController(private val timeBasedTokenService: TimeBasedTokenService) {
     @PostMapping("/{useIdSessionId}/tokens", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Operation(summary = "Create time-based token for this session")
     @ApiResponse(responseCode = "200")
     @ApiResponse(responseCode = "400", description = "Invalid useIdSessionId (session not found)", content = [Content()])
     fun getTimeBasedToken(@PathVariable useIdSessionId: UUID): Mono<ResponseEntity<TimeBasedToken>> {
@@ -43,6 +45,7 @@ class TimeBasedTokenController(private val timeBasedTokenService: TimeBasedToken
     }
 
     @GetMapping("/{useIdSessionId}/tokens/{tokenId}")
+    @Operation(summary = "Check if time-based token for this session is (still) valid")
     @ApiResponse(responseCode = "204", description = "Token was found and is valid", content = [Content()])
     @ApiResponse(responseCode = "404", description = "Token was not found (or not valid anymore)", content = [Content()])
     fun timeBasedTokenValidation(@PathVariable useIdSessionId: UUID, @PathVariable tokenId: UUID): Mono<ResponseEntity<Nothing>> {
