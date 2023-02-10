75539d0173cb263b866b8f8fe59d66e415a4e01d
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2023-02-10 10:13:11 +0100

    Handover

--- src/main/kotlin/de/bund/digitalservice/useid/apikeys/ApiKeyAuthenticationFilter.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/apikeys/ApiKeyAuthenticationFilter.kt
@@ -1,10 +1,13 @@
 package de.bund.digitalservice.useid.apikeys
 
+import de.bund.digitalservice.useid.identification.IDENTIFICATION_SESSIONS_BASE_PATH
 import org.springframework.http.HttpHeaders
+import org.springframework.http.server.PathContainer
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
+import org.springframework.web.util.pattern.PathPatternParser
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -17,8 +20,20 @@ class ApiKeyAuthenticationFilter(private val authenticationManager: Authenticati
         request: HttpServletRequest,
         response: HttpServletResponse
     ): Authentication {
-        val authHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
-        if (authHeader.trim().isEmpty() || !authHeader.trim().startsWith(AUTH_HEADER_VALUE_PREFIX)) {
+        val listOfPages = listOf(
+            PathPatternParser().parse("$IDENTIFICATION_SESSIONS_BASE_PATH/*/tc-token"),
+            PathPatternParser().parse("$IDENTIFICATION_SESSIONS_BASE_PATH/*/tokens/*"),
+            PathPatternParser().parse("$IDENTIFICATION_SESSIONS_BASE_PATH/*/tokens"),
+            PathPatternParser().parse("$IDENTIFICATION_SESSIONS_BASE_PATH/*/transaction-info") // TODO: GET should not be authenticated, but POST should be
+        )
+        val pathShouldNotBeAuthenticated = listOfPages.any {
+            it.matches(PathContainer.parsePath(request.servletPath))
+        }
+        if (pathShouldNotBeAuthenticated) {
+            return ApiKeyAuthenticationToken("")
+        }
+        val authHeader: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
+        if (authHeader == null || authHeader.trim().isEmpty() || !authHeader.trim().startsWith(AUTH_HEADER_VALUE_PREFIX)) {
             throw BadCredentialsException("API Key is missing.")
         }
         val token = authHeader.substring(AUTH_HEADER_VALUE_PREFIX.length)
