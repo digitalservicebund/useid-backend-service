b3200c49fa7e16c7dfd90b869803560e66c189a0
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-02-17 17:34:30 +0100

    USEID-744: Use OncePerRequestFilte SecurityHeadersFilter

--- src/main/kotlin/de/bund/digitalservice/useid/config/SecurityHeadersFilter.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/config/SecurityHeadersFilter.kt
@@ -3,42 +3,38 @@ package de.bund.digitalservice.useid.config
 import de.bund.digitalservice.useid.widget.WIDGET_PAGE
 import org.springframework.http.HttpHeaders
 import org.springframework.http.server.PathContainer
-import org.springframework.web.filter.GenericFilterBean
+import org.springframework.web.filter.OncePerRequestFilter
 import org.springframework.web.util.pattern.PathPattern
 import org.springframework.web.util.pattern.PathPatternParser
 import javax.servlet.FilterChain
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class SecurityHeadersFilter(
     private val contentSecurityPolicyProperties: ContentSecurityPolicyProperties
-) : GenericFilterBean() {
+) : OncePerRequestFilter() {
 
     private val widgetPagePath: PathPattern = PathPatternParser().parse("/$WIDGET_PAGE")
     private val listOfPages = listOf(widgetPagePath)
 
-    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-        val httpRequest = request as HttpServletRequest
-        val httpResponse = response as HttpServletResponse
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
         val pathIsValidWidgetPages = listOfPages.any {
-            it.matches(PathContainer.parsePath(httpRequest.servletPath))
+            it.matches(PathContainer.parsePath(request.servletPath))
         }
 
-        if (!pathIsValidWidgetPages) return chain.doFilter(request, response)
+        if (!pathIsValidWidgetPages) return filterChain.doFilter(request, response)
 
-        val hostName: String? = httpRequest.getParameter("hostname")
+        val hostName: String? = request.getParameter("hostname")
         val hostNameIsAllowed = hostName?.let { contentSecurityPolicyProperties.domainIsAllowed(it) }
 
         if (hostNameIsAllowed == true) {
-            httpResponse.setHeader("Content-Security-Policy", contentSecurityPolicyProperties.getCSPHeaderValue(hostName))
-            httpResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, hostName)
-            httpResponse.setHeader(HttpHeaders.VARY, HttpHeaders.ORIGIN)
+            response.setHeader("Content-Security-Policy", contentSecurityPolicyProperties.getCSPHeaderValue(hostName))
+            response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, hostName)
+            response.setHeader(HttpHeaders.VARY, HttpHeaders.ORIGIN)
         } else {
-            httpResponse.setHeader("Content-Security-Policy", contentSecurityPolicyProperties.getDefaultCSPHeaderValue())
+            response.setHeader("Content-Security-Policy", contentSecurityPolicyProperties.getDefaultCSPHeaderValue())
         }
 
-        return chain.doFilter(request, response)
+        return filterChain.doFilter(request, response)
     }
 }
