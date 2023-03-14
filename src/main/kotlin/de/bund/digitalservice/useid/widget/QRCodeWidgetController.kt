86d4488339b99741b4b038eb999f7c4b8a4abae8
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-03-14 18:07:34 +0100

    Add App simulator and cleanup code

--- src/main/kotlin/de/bund/digitalservice/useid/widget/QRCodeWidgetController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/widget/QRCodeWidgetController.kt
@@ -8,13 +8,15 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
+import java.util.UUID
 
 internal const val QRCODE_WIDGET_PAGE = "qrcode-widget"
+internal const val APP_SIMULATOR_PAGE = "app-simulator"
 
 @Controller
 @Timed
 @ConditionalOnProperty(name = ["features.desktop-solution-enabled"], havingValue = "true")
-class DesktopWidgetController(
+class QRCodeWidgetController(
     private val applicationProperties: ApplicationProperties,
 ) {
     @GetMapping("/$QRCODE_WIDGET_PAGE")
@@ -32,6 +34,24 @@ class DesktopWidgetController(
         )
 
         val modelAndView = ModelAndView(QRCODE_WIDGET_PAGE)
+        modelAndView.addAllObjects(widgetViewConfig)
+
+        return modelAndView
+    }
+
+    @GetMapping("/$APP_SIMULATOR_PAGE")
+    fun getAppSimulatorPage(
+        model: Model,
+        @RequestParam widgetSessionId: UUID,
+    ): ModelAndView {
+        // no tracking for desktop solution while prototyping
+
+        val widgetViewConfig = mapOf(
+            "baseUrl" to applicationProperties.baseUrl,
+            "widgetSessionId" to widgetSessionId,
+        )
+
+        val modelAndView = ModelAndView(APP_SIMULATOR_PAGE)
         modelAndView.addAllObjects(widgetViewConfig)
 
         return modelAndView
