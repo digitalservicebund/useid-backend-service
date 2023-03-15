a8bd3ecf0433ff1ffeb71d3f79d4facbb4c6eab1
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-03-15 16:40:33 +0100

    Parse host from base URL

--- src/main/kotlin/de/bund/digitalservice/useid/webauthn/RelyingPartyConfig.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/webauthn/RelyingPartyConfig.kt
@@ -6,6 +6,7 @@ import de.bund.digitalservice.useid.config.ApplicationProperties
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.net.URI
 
 // documentation: https://github.com/Yubico/java-webauthn-server#2-instantiate-a-relyingparty
 @Configuration
@@ -15,7 +16,7 @@ class RelyingPartyConfig(val userCredentialService: UserCredentialService, val a
     @Bean
     fun relyingParty(): RelyingParty {
         val rpIdentity = RelyingPartyIdentity.builder()
-            .id(applicationProperties.baseUrl) // Set this to a parent domain that covers all subdomains where users' credentials should be valid
+            .id(URI(applicationProperties.baseUrl).host) // Set this to a parent domain that covers all subdomains where users' credentials should be valid
             .name("BundesIdent")
             .build()
 
