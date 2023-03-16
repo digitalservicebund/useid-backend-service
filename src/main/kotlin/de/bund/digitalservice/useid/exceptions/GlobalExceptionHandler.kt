8b245a498e33815db77a4d0854431fe2d18702cc
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-03-16 15:36:27 +0100

    Fix global exception handling
    Do not override Spring default behavior

--- src/main/kotlin/de/bund/digitalservice/useid/exceptions/GlobalExceptionHandler.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/exceptions/GlobalExceptionHandler.kt
@@ -9,10 +9,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
-class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
+class GlobalExceptionHandler {
     private val log = KotlinLogging.logger {}
 
     @ExceptionHandler(WidgetNotFoundException::class)
