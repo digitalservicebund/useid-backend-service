b789150e64ceece9cd2a5425469d3f1f34267e9d
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2023-03-17 11:23:49 +0100

    Revert "Revert "USEID-768: Migrate to Spring Boot 3 (#221)""
    This reverts commit 8f6f24e44c73bdc95e1ad7451509d23f3d7c63c4.

--- src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidServiceProperties.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidServiceProperties.kt
@@ -1,11 +1,11 @@
 package de.bund.digitalservice.useid.eidservice
 
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 import org.springframework.validation.annotation.Validated
-import javax.validation.Valid
-import javax.validation.constraints.NotBlank
 
 @Component
 @ConfigurationProperties(prefix = "eidservice")
