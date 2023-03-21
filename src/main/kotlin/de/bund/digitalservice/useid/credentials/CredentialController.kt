3d85b53aca3d38e9e176f5a5f836dff5d472c7ca
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-03-21 16:53:09 +0100

    Add swagger docs

--- src/main/kotlin/de/bund/digitalservice/useid/credentials/CredentialController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/credentials/CredentialController.kt
@@ -3,6 +3,11 @@ package de.bund.digitalservice.useid.credentials
 import de.bund.digitalservice.useid.events.AuthenticateEvent
 import de.bund.digitalservice.useid.events.EventService
 import de.bund.digitalservice.useid.events.EventType
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.parameters.RequestBody
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -10,7 +15,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
@@ -20,11 +24,17 @@ internal const val CREDENTIALS_BASE_PATH = "/api/v1/credentials"
 @RestController
 @RequestMapping(CREDENTIALS_BASE_PATH)
 @ConditionalOnProperty(name = ["features.desktop-solution-enabled"], havingValue = "true")
+@Tag(
+    name = "Credentials",
+    description = "WebAuthn credentials are used to authenticate the a user in the widget.",
+)
 class CredentialController(
     private val credentialService: CredentialService,
     private val eventService: EventService,
 ) {
     @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Operation(summary = "Start registration for WebAuthn credentials.")
+    @ApiResponse(responseCode = "201", content = [Content()])
     fun startRegistration(
         @RequestBody startRegistrationRequest: StartRegistrationRequest,
     ): ResponseEntity<Any> {
@@ -47,9 +57,11 @@ class CredentialController(
     @PutMapping(
         path = ["/{credentialId}"],
     )
+    @Operation(summary = "Complete registration for WebAuthn credentials and start authentication. Notifies the widget about the started authentication.")
+    @ApiResponse(responseCode = "204", content = [Content()])
     fun completeRegistration(
         @PathVariable credentialId: UUID,
-        @RequestBody publicKeyCredentialJson: String,
+        @RequestBody(description = "PublicKeyCredential returned after registration as described here: https://webauthn.guide/#registration") publicKeyCredentialJson: String,
     ): ResponseEntity<Void> {
         credentialService.finishRegistration(credentialId, publicKeyCredentialJson)
 
@@ -73,9 +85,11 @@ class CredentialController(
         path = ["/{credentialId}/authentications"],
         produces = [MediaType.APPLICATION_JSON_VALUE],
     )
+    @Operation(summary = "Complete authentication with WebAuthn credentials.")
+    @ApiResponse(responseCode = "200", content = [Content()])
     fun completeAuthentication(
         @PathVariable credentialId: UUID,
-        @RequestBody publicKeyCredentialJson: String,
+        @RequestBody(description = "PublicKeyCredential returned after authentication as described here: https://webauthn.guide/#authentication") publicKeyCredentialJson: String,
     ): ResponseEntity<Any> {
         val refreshAddress = credentialService.finishAuthentication(credentialId, publicKeyCredentialJson)
         return ResponseEntity
