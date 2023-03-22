2efe5617a5dfa1a5c238933f39a29bc7d41147b3
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-03-22 14:37:56 +0100

    Spotless apply

--- src/main/kotlin/de/bund/digitalservice/useid/credentials/CredentialController.kt
+++ src/main/kotlin/de/bund/digitalservice/useid/credentials/CredentialController.kt
@@ -5,7 +5,6 @@ import de.bund.digitalservice.useid.events.EventService
 import de.bund.digitalservice.useid.events.EventType
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
-import org.springframework.web.bind.annotation.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -15,6 +14,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
