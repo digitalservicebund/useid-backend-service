bcc6d12ac8ffbac598c981e6dfbb7691cbe6505e
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-03-22 13:39:34 +0100

    NO-TICKET: Add missing dependency
    By accident removed the whole dependency instead of just the pinned version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
     /** Persistence **/
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
     implementation("org.flywaydb:flyway-core:9.16.0")
     implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.2.0")
 
