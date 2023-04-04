7d66fd8298a6100302e493f3c2868b2b93d71403
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-04-04 16:07:08 +0200

    USEID-696: Fix DB migration script
    Add default value to satisfy not null constraint of existing entries. Drop default again immediately.

--- src/main/resources/db/migration/V3__Add_TenantId_To_Session.sql
+++ src/main/resources/db/migration/V3__Add_TenantId_To_Session.sql
@@ -1,2 +1,2 @@
-ALTER TABLE identification_session
-ADD tenant_id VARCHAR(50) NOT NULL;
+ALTER TABLE identification_session ADD tenant_id VARCHAR(50) NOT NULL DEFAULT 'Grundsteuer';
+ALTER TABLE identification_session ALTER COLUMN tenant_id DROP DEFAULT;
