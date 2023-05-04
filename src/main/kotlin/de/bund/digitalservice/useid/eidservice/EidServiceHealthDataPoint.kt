172b2055d61c4caeed818becc8001899544f437e
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-05-04 12:30:06 +0200

    USEID-811: Add EidService Redis Repository

---
+++ src/main/kotlin/de/bund/digitalservice/useid/eidservice/EidServiceHealthDataPoint.kt
@@ -0,0 +1,8 @@
+package de.bund.digitalservice.useid.eidservice
+
+import org.springframework.data.redis.core.RedisHash
+import java.io.Serializable
+import java.util.Date
+
+@RedisHash("EidServiceHealth")
+data class EidServiceHealthDataPoint(val id: String, val up: Boolean, val timestamp: Date) : Serializable
