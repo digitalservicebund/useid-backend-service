de0c68d8cef192efbc332a904b9c6e3278a24ed7
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-05-07 13:10:08 +0200

    USEID-811: Rename tests

--- src/test/kotlin/de/bund/digitalservice/useid/eidservice/EidHealthServiceIntegrationTest.kt
+++ src/test/kotlin/de/bund/digitalservice/useid/eidservice/EidHealthServiceIntegrationTest.kt
@@ -18,7 +18,7 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootTest
 @Tag("integration")
 @Transactional
-class EidServiceHealthTestSchedulerIntegrationTest() {
+class EidHealthServiceIntegrationTest() {
 
     @Autowired
     private lateinit var eidServiceRepository: EidServiceRepository
