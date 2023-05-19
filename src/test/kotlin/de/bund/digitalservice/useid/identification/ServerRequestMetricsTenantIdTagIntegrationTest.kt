111add4602060d9200d7ae7a02484652b247d665
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2023-05-19 10:00:20 +0200

    Refactored server request metrics integration test

--- src/test/kotlin/de/bund/digitalservice/useid/identification/ServerRequestMetricsTenantIdTagIntegrationTest.kt
+++ src/test/kotlin/de/bund/digitalservice/useid/identification/ServerRequestMetricsTenantIdTagIntegrationTest.kt
@@ -2,6 +2,8 @@ package de.bund.digitalservice.useid.identification
 
 import de.bund.digitalservice.useid.eidservice.EidService
 import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Tag
@@ -19,73 +21,70 @@ import java.util.UUID
 @TestPropertySource(properties = ["management.endpoints.web.exposure.include=prometheus"])
 class ServerRequestMetricsTenantIdTagIntegrationTest(@Autowired val webTestClient: WebTestClient) {
 
+    val expectedTenantId = "integration_test_1"
+
     @BeforeAll
-    fun setup() {
+    fun beforeAll() {
         mockkConstructor(EidService::class)
+    }
+
+    @AfterAll
+    fun afterAll() {
+        unmockkAll()
     }
 
     @Test
     fun `prometheus log contains server request metric with tenant ID for start session endpoint after start session call`() {
-        val expectedTenantId = "integration_test_1"
+        // Given
         val expectedPrometheusLogRegex = Regex("http_server_requests_seconds_count\\{error=\"none\",exception=\"none\",method=\"POST\",outcome=\"SUCCESS\",status=\"200\",tenant_id=\"(.+)\",uri=\"/api/v1/identifications\",}")
 
+        // When
         webTestClient.sendStartSessionRequest()
 
-        webTestClient.createGETRequest("actuator/prometheus")
-            .exchange()
-            .expectStatus().isOk
-            .expectBody()
-            .returnResult()
-            .responseBody?.let { prometheusLogRaw ->
-            val prometheusLog = String(bytes = prometheusLogRaw)
-            val result = expectedPrometheusLogRegex.find(prometheusLog)
-            val tenantId = result?.groupValues?.get(1)
-
-            Assertions.assertEquals(expectedTenantId, tenantId)
-        }
+        // Then
+        val tenantId = getTenantIdFromPrometheusLog(expectedPrometheusLogRegex)
+        Assertions.assertEquals(expectedTenantId, tenantId)
     }
 
     @Test
     fun `prometheus log contains server request metric with tenant ID for tc token endpoint after tc token is requested`() {
-        var tcTokenURL = ""
-        val expectedTenantId = "integration_test_1"
+        // Given
         val expectedPrometheusLogRegex = Regex("http_server_requests_seconds_count\\{error=\"none\",exception=\"none\",method=\"GET\",outcome=\"SUCCESS\",status=\"200\",tenant_id=\"(.+)\",uri=\"/api/v1/tc-tokens/\\{useIdSessionId}\",}")
 
+        // When
+        var tcTokenURL = ""
         webTestClient.sendStartSessionRequest()
             .expectBody().jsonPath("$.tcTokenUrl").value<String> { tcTokenURL = it }
-
         val eIdSessionId = UUID.randomUUID()
-
         mockTcToken("https://www.foobar.com?sessionId=$eIdSessionId")
-
         webTestClient.createGETRequest(extractRelativePathFromURL(tcTokenURL))
             .exchange()
 
-        webTestClient.get().uri("actuator/prometheus")
-            .exchange()
-            .expectStatus().isOk
-            .expectBody()
-            .returnResult()
-            .responseBody?.let { prometheusLogRaw ->
-            val prometheusLog = String(bytes = prometheusLogRaw)
-            val result = expectedPrometheusLogRegex.find(prometheusLog)
-            val tenantId = result?.groupValues?.get(1)
-
-            Assertions.assertEquals(expectedTenantId, tenantId)
-        }
+        // Then
+        val tenantId = getTenantIdFromPrometheusLog(expectedPrometheusLogRegex)
+        Assertions.assertEquals(expectedTenantId, tenantId)
     }
 
     @Test
     fun `prometheus log contains server request metric with tenant ID for widget endpoint after widget is requested`() {
-        val expectedTenantId = "integration_test_1"
+        // Given
         val expectedPrometheusLogRegex = Regex("http_server_requests_seconds_count\\{error=\"none\",exception=\"none\",method=\"GET\",outcome=\"SUCCESS\",status=\"200\",tenant_id=\"(.+)\",uri=\"/widget\",}")
         val allowedHost = "i.am.allowed.1"
 
+        // When
         webTestClient.createGETRequest("/widget?hostname=$allowedHost")
             .exchange()
             .expectStatus().isOk
 
-        webTestClient.get().uri("actuator/prometheus")
+        // Then
+        val tenantId = getTenantIdFromPrometheusLog(expectedPrometheusLogRegex)
+        Assertions.assertEquals(expectedTenantId, tenantId)
+    }
+
+    private fun getTenantIdFromPrometheusLog(expectedPrometheusLogRegex: Regex): String? {
+        var tenantId: String? = ""
+        webTestClient
+            .createGETRequest("actuator/prometheus")
             .exchange()
             .expectStatus().isOk
             .expectBody()
@@ -93,9 +92,8 @@ class ServerRequestMetricsTenantIdTagIntegrationTest(@Autowired val webTestClien
             .responseBody?.let { prometheusLogRaw ->
             val prometheusLog = String(bytes = prometheusLogRaw)
             val result = expectedPrometheusLogRegex.find(prometheusLog)
-            val tenantId = result?.groupValues?.get(1)
-
-            Assertions.assertEquals(expectedTenantId, tenantId)
+            tenantId = result?.groupValues?.get(1)
         }
+        return tenantId
     }
 }
