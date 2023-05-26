7188cb41312a377bbad1d164be7bfceab4fa695f
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2023-05-26 18:20:11 +0200

    Try to overcome spotless

--- src/integrationTest/kotlin/de/bund/digitalservice/useid/tenant/RequestMetricsTenantIdTagIntegrationTest.kt
+++ src/integrationTest/kotlin/de/bund/digitalservice/useid/tenant/RequestMetricsTenantIdTagIntegrationTest.kt
@@ -7,6 +7,7 @@ import de.bund.digitalservice.useid.identification.sendGETRequest
 import de.bund.digitalservice.useid.identification.sendStartSessionRequest
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
+import mu.KotlinLogging
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
@@ -78,12 +79,11 @@ class RequestMetricsTenantIdTagIntegrationTest(@Autowired val webTestClient: Web
             .expectStatus().isOk
 
         // Then
-        val tenantId = getTenantIdFromPrometheusLog(expectedPrometheusLogRegex, debug = true)
+        val tenantId = getTenantIdFromPrometheusLog(expectedPrometheusLogRegex, printLog = true)
         assertThat(tenantId).isEqualTo(expectedTenantId)
     }
 
-    private fun getTenantIdFromPrometheusLog(expectedPrometheusLogRegex: Regex, debug: Boolean = false): String? {
-        // val log = KotlinLogging.logger {}
+    private fun getTenantIdFromPrometheusLog(expectedPrometheusLogRegex: Regex, printLog: Boolean = false): String? {
         var tenantId: String? = ""
         webTestClient
             .sendGETRequest("actuator/prometheus")
@@ -92,14 +92,14 @@ class RequestMetricsTenantIdTagIntegrationTest(@Autowired val webTestClient: Web
             .returnResult()
             .responseBody?.let { prometheusLogRaw ->
             val prometheusLog = String(bytes = prometheusLogRaw)
-
-            //     if (debug) {
-            //     println("##### DEBUG LOG \n $prometheusLog")
-            //     log.info {
-            //         "##### DEBUG LOG \n" +
-            //             " $prometheusLog"
-            //     }
-            // }
+            if (printLog) {
+                val log = KotlinLogging.logger {}
+                println("##### DEBUG LOG \n $prometheusLog")
+                log.info {
+                    "##### DEBUG LOG \n" +
+                        " $prometheusLog"
+                }
+            }
             val result = expectedPrometheusLogRegex.find(prometheusLog)
             tenantId = result?.groupValues?.get(1)
         }
